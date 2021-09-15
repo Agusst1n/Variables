f21a711d9f087211e4f13e44f2db55c0e517bf61
Nitsuga <88418234+Agusst1n@users.noreply.github.com>
2021-09-15 16:14:41 -0300

    ejercicio2Resuelto

--- ejercicios2.js
+++ ejercicios2.js
@@ -6,8 +6,16 @@ console.log (apellido)
 var saludo = nombre + " " + apellido;
 alert ("Hola" + " " + saludo)
 
-
+alert("Bueno ahora vamos a hacer una mini suma")
 var primerNumero = window.prompt("Introduzca el primero valor <3");
 var segundoNumero = window.prompt("Introduzca el segundo valor OwO *c enoja*");
 var resultado = parseInt(primerNumero) + parseInt(segundoNumero)
 alert("Hola querido usuario tu resultado es onichan : "+ resultado)
+
+alert("ahora hacela facil y decime en que año naciste *pone el fierro sobre la mesa 🔫*")
+alert("Te la creiste wey xd")
+
+var añoActual = 2021; 
+var edad = window.prompt("Bueno ahora si, ¿Cuanto cumpliste este año?")
+var nacimiento = parseInt(añoActual)  - parseInt(edad);
+alert("Uh estas re viejo naciste en el: " + nacimiento);
