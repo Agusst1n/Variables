daf26270de2428448d3d6fd5d95bc7e87cf4a510
Angélica Sofía Pagés Burgos <68571768+angelicapages@users.noreply.github.com>
2021-09-16 12:25:28 -0300

    ejercicios terminados

--- app.js
+++ app.js
@@ -11,33 +11,4 @@ var segundoNumero=window.prompt("¿Ingrese otro numero por favor?");
 var suma=parseInt(primerNumero) + parseInt( segundoNumero);
 alert("El resultado de la suma es:"+" "+ suma);
 
-var balance= 2300;
-alert("Su balance es de: $" + balance);
-var retirar=window.prompt("¿Cuanta plata desea retirar?");
-var cuenta= parseInt(balance) - parseInt(retirar);
-var suCuenta= alert("la plata que le queda en la cuenta es: $" + cuenta);
-
-var primerNota=window.prompt("¿Que te sacaste en el primer examen?");
-var segundarNota=window.prompt("¿Que te sacaste en el segundo examen?");
-var tercerNota=window.prompt("¿Que te sacaste en el tercer examen?");
-var promedio= (parseInt(primerNota) + parseInt(segundarNota) + parseInt(tercerNota)) /3;
-var suPromedio= alert("Tinen promedio de:"+" "+ promedio);
-
-var numeroMetros=window.prompt("Ingresa una distancia en metros");
-var pies= parseInt(numeroMetros)*(3.28084);
-var centimetros= parseInt(numeroMetros)*(100);
-var pulgadas=parseInt(numeroMetros)*(39.3701);
-var cartel=alert((numeroMetros +" "+ "metros")+", "+("son"+" "+ pies +" "+"pies")+ ", "+ (centimetros+" "+ "centimetros")+", "+("y"+" "+ pulgadas+" "+ "pulgadas"));
-
-
-var PrimerNumero=window.prompt("¿Ingrese un numero por favor");
-var SegundoNumero=window.prompt("¿Ingrese un segundo numero por favor?");
-var calculo=parseInt(PrimerNumero) + parseInt( SegundoNumero);
-alert("El resultado de la suma es:"+" "+ calculo);
-var TercerNumero=window.prompt("¿Ingrese un tercer numero por favor?");
-var calculo2=parseInt(TercerNumero)*calculo;
-alert("El resultado de la cuenta es:"+" "+ calculo2);
-
-var minutos=window.prompt("¿Ingrese una cantidad de minutos");
-var pasajeSegundos=parseInt(minutos)*(60);
-alert(minutos +" " +"minutos"+ " "+ "son" +" "+ pasajeSegundos+" "+"segundos");*/
+/*
--- ejercicios2.js
+++ ejercicios2.js
@@ -1,14 +1,100 @@
-/*function bienvenida (){
-    var nombre= window.prompt ("¿Cómo te llamas?");
-    console.log (nombre)  
-    var apellido= window.prompt ("¿Cual es tu apellido?");
-    console.log (apellido)
-    var saludo = nombre + " " + apellido;
-    alert ("Hola" + " " + saludo)
-}
-bienvenida();*/
+// EJERCICIO 1
+// function bienvenida() {
+//     var nombre = window.prompt("¿Cómo te llamas?");
+//     console.log(nombre)
+//     var apellido = window.prompt("¿Cual es tu apellido?");
+//     console.log(apellido)
+//     var saludo = nombre + " " + apellido;
+//     alert("Hola" + " " + saludo)
+// }
+// bienvenida();
 
-var primerNumero = window.prompt("Introduzca el primero valor <3");
-var segundoNumero = window.prompt("Introduzca el segundo valor OwO *c enoja*");
-var resultado = parseInt(primerNumero) + parseInt(segundoNumero)
-alert("Hola querido usuario tu resultado es onichan : "+ resultado)
+// EJERCICIO 2
+// var primerNumero = window.prompt("Introduzca el primero valor <3");
+// var segundoNumero = window.prompt("Introduzca el segundo valor OwO *c enoja*");
+// var resultado = parseInt(primerNumero) + parseInt(segundoNumero)
+// alert("Hola querido usuario tu resultado es onichan : " + resultado)
+
+// EJERCICIO 3
+// var balance = 2300;
+// alert("Su balance es de: $" + balance);
+// var retirar = window.prompt("¿Cuanta plata desea retirar?");
+// var cuenta = parseInt(balance) - parseInt(retirar);
+// var suCuenta = alert("la plata que le queda en la cuenta es: $" + cuenta);
+
+// EJERCICIO 4
+// var primerNota = window.prompt("¿Que te sacaste en el primer examen?");
+// var segundarNota = window.prompt("¿Que te sacaste en el segundo examen?");
+// var tercerNota = window.prompt("¿Que te sacaste en el tercer examen?");
+// var promedio = (parseInt(primerNota) + parseInt(segundarNota) + parseInt(tercerNota)) / 3;
+// var suPromedio = alert("Tinen promedio de:" + " " + promedio);
+
+// EJERCICIO 5
+// var numeroMetros = window.prompt("Ingresa una distancia en metros");
+// var pies = parseInt(numeroMetros) * (3.28084);
+// var centimetros = parseInt(numeroMetros) * (100);
+// var pulgadas = parseInt(numeroMetros) * (39.3701);
+// var cartel = alert((numeroMetros + " " + "metros") + ", " + ("son" + " " + pies + " " + "pies") + ", " + (centimetros + " " + "centimetros") + ", " + ("y" + " " + pulgadas + " " + "pulgadas"));
+
+// EJERCICIO 6
+// var numero1 = window.prompt("Ingrese un numero por favor");
+// var numero2 = window.prompt("Ingrese un segundo numero por favor");
+// var calculo = parseInt(numero1) + parseInt(numero2);
+// alert("El resultado de la suma es:" + " " + calculo);
+// var numero3 = window.prompt("Ingrese un tercer numero por favor");
+// var calculo2 = parseInt(numero3) * calculo;
+// alert("El resultado de la cuenta es:" + " " + calculo2);
+
+// EJERCICIO 7
+// var minutos = window.prompt("¿Ingrese una cantidad de minutos");
+// var pasajeSegundos = parseInt(minutos) * (60);
+// alert(minutos + " " + "minutos" + " " + "son" + " " + pasajeSegundos + " " + "segundos");
+
+// EJERCICIO 8
+// var baseRec = window.prompt("Ingrese número de la base del rectángulo por favor");
+// var alturaRec = window.prompt("Ingrese el número de la altura del rectángulo por favor");
+// var calculoRec = parseInt(baseRec) * parseInt(alturaRec);
+// alert("El área del rectángulo es" + " " + calculoRec);
+
+// EJERCICIO 9
+// var baseTri = window.prompt("Ingrese número de la base del triángulo por favor");
+// var alturaTri = window.prompt("Ingrese el número de la altura del triángulo por favor");
+// var calculoTri = parseInt(baseTri) * parseInt(alturaTri) / 2;
+// alert("El área del triángulo es" + " " + calculoTri);
+
+// EJERCICIO 10
+// var monto = window.prompt("Ingrese número de monto por favor");
+// var descuento = window.prompt("Ingrese número de descuento por favor");
+// var total = parseInt(monto) - parseInt(descuento);
+// alert("El monto total es" + " " + total);
+
+// EJERCICIO 11
+// var nombreCliente = window.prompt("Ingrese su nombre por favor");
+// var edad = window.prompt("Ingrese su edad por favor");
+// var añoActual = window.prompt("Ingrese el año actual por favor");
+// var año = parseInt(añoActual) - parseInt(edad);
+// alert("Te llamas" + " " + nombreCliente + " " + " y tu año de nacimiento es" + " " + año);
+
+// EJERCICIO 12
+// var clienteNombre = window.prompt("Ingrese su nombre");
+// var celsius = window.prompt("Ingrese los grados celsius");
+// var farenheit = parseInt(celsius) + 32;
+// console.log("Hola" + " " + clienteNombre + "," + " " + "Los grados farenheit son" + " " + farenheit + " " + " que equivalen a los siguientes grados celsius:" + " " + celsius);
+
+// EJERCICIO 13
+// var clienteNombre2 = window.prompt("Ingrese su nombre");
+// var farenheit2 = window.prompt("Ingrese los grados farenheit");
+// var celsius2 = parseInt(farenheit2) - 32;
+// console.log("Hola" + " " + clienteNombre2 + "," + " " + "Los grados celsius son" + " " + celsius2 + " " + " que equivalen a los siguientes grados farenheit:" + " " + farenheit2);
+
+// EJERCICIO 13
+// var numeroPedido = window.prompt("Ingrese un número con decimales")
+// var calculoRequerido = ((parseInt(numeroPedido)+ 5) * 10) / 3;
+// var resultadoFinal = Math.round(calculoRequerido)
+// console.log(resultadoFinal)
+
+// EJERCICIO 14
+// var numeroPedido2 = window.prompt("Ingrese un número")
+// var numeroPedido3 = window.prompt("Ingrese otro número uwu")
+// var numeroFinal = numeroPedido2 + numeroPedido3
+// console.log(numeroFinal)
